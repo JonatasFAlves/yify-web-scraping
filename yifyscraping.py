a9b722a9cc3f9da6929e875d79fa2ccb3f1a6dd4
JonatasFAlves <alves.jnts@gmail.com>
2018-08-06 23:47:35 -0300

    Write movies to movies.csv

--- yifyscraping.py
+++ yifyscraping.py
@@ -1,4 +1,4 @@
-import requests
+import requests, csv
 from bs4 import BeautifulSoup
 
 
@@ -11,33 +11,45 @@ def get_imdb_rating(yify_detail_url):
     rating = rating_div.find('span', itemprop='ratingValue')
     rating_count = rating_div.find('span', itemprop='ratingCount')
     return [rating.string, rating_count.string]
-    
-# IMDB ratings are accepted if there are at least 10000 votes
-def acceptable_imdb_rating(imdb):
-    ratingCount = int(imdb[1])
-    return ratingCount > 10000
+
+def acceptable_imdb_rating(imdb_counting):
+    return int(imdb_counting) > 10000
 
 main_page_response = requests.get('https://yts.am/browse-movies/0/all/sci-fi/7/year')
 main_soup = BeautifulSoup(main_page_response.text, 'html.parser')
 
 movies_in_page = main_soup.find_all('div', class_='browse-movie-wrap')
 
-for movie in movies_in_page:
-    movie_bottom = movie.div
-    name_year = movie_bottom.a.string + ' - ' + movie_bottom.div.string
+with open('movies.csv', 'w', newline='') as moviefile:
+    fieldnames = ['Movie', 'Year', 'Genre', 'Rating', 'Rating Count']
+    writer = csv.DictWriter(moviefile, fieldnames=fieldnames)
 
-    movie_link = movie.a
-    attributes = [attr.string for attr in movie_link.figure.figcaption.find_all('h4') if not attr.get('class')]
-    str_attributes = ', '.join(attributes)
+    writer.writeheader()
 
-    try:
-        imdb = get_imdb_rating(movie_link['href'])
-    except AttributeError:
-        print('Detail page not found for movie %s.' % name_year, '\n')
-        continue
+    for movie in movies_in_page:
+        movie_bottom = movie.div
 
-    if acceptable_imdb_rating(imdb):
-        print('Film: ', name_year)
-        print('Genre: ', str_attributes)
-        print('Imdb: [Rating: %s, RatingCount: %s]' % (imdb[0], imdb[1]))
-        print()
+        name = movie_bottom.a.string
+        year = movie_bottom.div.string
+
+        movie_link = movie.a
+        attributes = [attr.string for attr in movie_link.figure.figcaption.find_all('h4') if not attr.get('class')]
+        str_attributes = ' - '.join(attributes)
+
+        try:
+            imdb = get_imdb_rating(movie_link['href'])
+
+            imdb_rating = imdb[0]
+            imdb_counting = imdb[1]
+        except AttributeError:
+            print('Detail page not found for movie %s.' % name, '\n')
+            continue
+
+        if acceptable_imdb_rating(imdb_counting):
+            writer.writerow({
+                'Movie': name,
+                'Year': year,
+                'Genre': str_attributes,
+                'Rating': imdb_rating,
+                'Rating Count': imdb_counting
+            })
